cde34c4c59f4a0de5c63a32826613aab9a065497
anh_pham <>
2023-10-25 22:11:52 +0700

    update

---
+++ test/run_docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build --tag=0905062524/flaskapp:1.0 . 
+# docker build --tag=0905062524/api . 
+
+# Step 2: 
+# List docker images
+docker image ls
+
+# Step 3: 
+# Run flask app
+docker run -p 8000:80 0905062524/flaskapp:1.0
